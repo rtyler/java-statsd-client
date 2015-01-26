c83f162a94b9dbfbfb6d65cdbd2b621b9811bbca
R. Tyler Croy <tyler@monkeypox.org>
2015-01-26 00:58:48 -0800

    Refactor the DummyStatsDServer out into a standalone class to be used across test cases

--- src/test/java/com/timgroup/statsd/NonBlockingStatsDClientTest.java
+++ src/test/java/com/timgroup/statsd/NonBlockingStatsDClientTest.java
@@ -6,12 +6,6 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.startsWith;
 
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.SocketException;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -201,43 +195,5 @@ public final class NonBlockingStatsDClientTest {
             nullPrefixClient.stop();
         }
         assertThat(server.messagesReceived(), contains(startsWith("mycount:")));
-    }
-
-    private static final class DummyStatsDServer {
-        private final List<String> messagesReceived = new ArrayList<String>();
-        private final DatagramSocket server;
-
-        public DummyStatsDServer(int port) {
-            try {
-                server = new DatagramSocket(port);
-            } catch (SocketException e) {
-                throw new IllegalStateException(e);
-            }
-            new Thread(new Runnable() {
-                @Override public void run() {
-                    try {
-                        final DatagramPacket packet = new DatagramPacket(new byte[256], 256);
-                        server.receive(packet);
-                        messagesReceived.add(new String(packet.getData(), Charset.forName("UTF-8")).trim());
-                    } catch (Exception e) { }
-                }
-            }).start();
-        }
-
-        public void stop() {
-            server.close();
-        }
-
-        public void waitForMessage() {
-            while (messagesReceived.isEmpty()) {
-                try {
-                    Thread.sleep(50L);
-                } catch (InterruptedException e) {}
-            }
-        }
-
-        public List<String> messagesReceived() {
-            return new ArrayList<String>(messagesReceived);
-        }
     }
 }
